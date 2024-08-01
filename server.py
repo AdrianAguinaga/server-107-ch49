f2ff7f936563c22b01c0ba5e499a0d1fdd4cb04d
nefersillo <adrian.aguinaga@uabc.edu.mx>
2024-07-31 20:08:36 -0700

    107-3

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
@@ -9,10 +10,49 @@ def home():
 @app.get("/about")
 def about():
     return "this is the about page rules"
-    
 
-@app.get("/login")
-def login():
-    return "plase provide your credentials"
+@app.get("/json")
+def name():
+    me = {"name": "Adrian"}
+    return json.dumps(me)
+
+product = []
+@app.post("/api/products")
+def save_products():
+    items = request.get_json()
+    product.append(items)
+    print (items)   
+    return json.dumps(items) 
+    
+@app.get("/api/products")
+def get_products():
+    return json.dumps(product)
+
+@app.put("/api/products/<int:index>")
+def update_products(index):
+    updated_products = request.get_json()
+    if 0<= index < len(product):
+        product[index] = updated_products
+        return json.dumps(updated_products)
+    else: 
+        return "that index does not exist"
+    
+@app.delete("/api/products/<int:index>")
+def delete_products(index):
+    delete_products = request.get_json
+    if 0<= index < len(product):
+        delete_products = product.pop(index)
+        return json.dumps(delete_products)
+    else: 
+        return "that index does not exist"
+
+@app.patch("/api/products/<int:index>")
+def patch_products(index):
+    patch_products = request.get_json
+    if 0<= index < len(product):
+        product(index).update(patch_products)
+        return json.dumps(delete_products)
+    else: 
+        return "that index does not exist"
 
 app.run(debug=True)
